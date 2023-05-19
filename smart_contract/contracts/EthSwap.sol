fc8aff88069ef4472a9313cc369dd36ea9a62806
Ravi Prakash Chaurasiya <113404805+rp7007@users.noreply.github.com>
2023-05-19 14:59:43 +0530

    initial commit

--- smart_contract/contracts/EthSwap.sol
+++ smart_contract/contracts/EthSwap.sol
@@ -5,7 +5,7 @@ import "./Token.sol";
 contract EthSwap {
   string public name = "EthSwap Instant Exchange";
   Token public token;
-  uint public rate = 1000;
+  uint public rate = 100;
 
   event TokensPurchased(
     address account,
